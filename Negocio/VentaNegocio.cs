139454410518cea04d3f07ac3490ea17e1fa205d
GBossetti <gonzalo.bossetti@gmail.com>
2022-10-31 10:19:10 -0300

    Se agrega clase VentaNegocio

---
+++ Negocio/VentaNegocio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dominio;
+
+namespace Negocio
+{
+    public class VentaNegocio
+    {
+        public List<Venta> listar()
+        {
+            List<Venta> lista = new List<Venta>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("--Consulta SQL");
+                datos.ejecutarLectura();
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
