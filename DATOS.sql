29ddecc7b3cc178c9b8c545967646c287b987e68
Andrea Nuñez <106264362+AndreNuez@users.noreply.github.com>
2022-12-04 13:32:26 -0300

    ScripBD
    Se agrega nuevo archivo con BD organizada.

--- DATOS.sql
+++ DATOS.sql
@@ -4,7 +4,9 @@ insert into Generos (Descripcion) values
 ('Infantil'),
 ('Aventuras'),
 ('Policial'),
-('Novela no ficción')
+('Novela no ficción'),
+('Thriller'),
+('Fantástico')
 
 insert into Libros (Titulo, Descripcion, Autor, Editorial, Precio, Stock, IdGenero, Portada, Estado) values
 ('Nuestra parte de noche', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Mariana Enríquez', 'Anagrama', 5350, 200, 1, 'https://www.anagrama-ed.es/uploads/media/portadas/0001/23/9931fa307ceb1ff16718890a064f7522d498e7ef.jpeg', 1),
@@ -12,3 +14,5 @@ insert into Libros (Titulo, Descripcion, Autor, Editorial, Precio, Stock, IdGene
 
 EXEC SP_AltaLibro 'La Chica del Tren', 'El Bestseller en el que se basa la película', 'Paula Hawkins', 'Planeta', 4500, 10, 7,
                             'https://http2.mlstatic.com/D_NQ_NP_629953-MLA46669897461_072021-O.jpg'
+
+
--- ScriptBD.sql
+++ ScriptBD.sql
@@ -1,9 +1,8 @@
-/*drop database ECOMMERCE
-go*/
-
-/*create database ECOMMERCE
+--CREACIÓN BD
+create database ECOMMERCE
 go
 
+--TABLAS PARA LIBROS
 create table Generos (
 	IdGenero smallint primary key identity (1,1),
 	Descripcion varchar(1000)
@@ -22,58 +21,17 @@ create table Libros (
 	Portada varchar (1000),
 	Estado bit not null
 	)
-go*/
-
-create table Administradores (
-	IdAdministrador smallint primary key identity (1,1),
-	Mail varchar(500) unique, 
-	Contraseña varchar (500) Not Null,
-	Nombres varchar (50) Not Null,
-	Apellidos varchar (50) Not Null
-)
-go
-
-create table Clientes (
-	IdCliente smallint primary key identity (1,1),
-	Mail varchar(500) unique, 
-	Contraseña varchar (500) Not Null,
-	Nombres varchar (100) Not Null,
-	Apellidos varchar (100) Not Null,
-	DNI varchar (50) not null unique,
-	Telefono varchar (100), 
-	Celular varchar (100),
-	Calle varchar (100),
-	Numero varchar (10),
-	Piso varchar (10),
-	Departamento varchar (10),
-	CP varchar (10),
-	Localidad varchar (100),
-	Provincia varchar (100)
-)
 go
 
 
-/*create table Carrito(
-	IdCarrito smallint primary key identity (1,1),
-	IdCliente smallint foreign key references Clientes (IdCliente)
-)
-go
-
-create table Ventas(
-	IdVenta smallint primary key identity (1,1),
-	IdCarrito smallint foreign key references Carrito (IdCarrito),
-	FormaPago varchar (100) not null,
-	Fecha date not null,
-	Estado varchar (100) not null
-)
-go*/
-
-/*create procedure SP_librosListar as
+--PROCEDIMIENTOS PARA LIBROS
+create procedure SP_librosListar as
 select l.id, l.Titulo, l.Descripcion, l.Autor, l.Editorial, l.Precio, l.Stock, g.IdGenero as Genero_ID, g.Descripcion as Genero_Desc, l.Portada, l.estado 
 from Libros l 
 inner join Generos g on l.IdGenero = g.IdGenero 
 where l.Estado = 1
 GO
+
 
 CREATE PROCEDURE SP_AltaLibro
     @Titulo varchar(200),
@@ -88,6 +46,7 @@ AS
 INSERT INTO Libros VALUES (@Titulo, @Descripcion, @Autor, @Editorial, @Precio, @Stock, @IdGenero, @PortadaURL, 1)
 GO 
 
+
 CREATE PROCEDURE SP_ModificarLibro 
     @Titulo varchar(200),
     @Descripcion varchar (1000),
@@ -101,139 +60,34 @@ CREATE PROCEDURE SP_ModificarLibro
 AS 
 UPDATE Libros SET Titulo= @Titulo, Descripcion= @Descripcion, Autor= @Autor, Editorial= @Editorial, Precio= @Precio, Stock= @Stock,
                   IDGenero= @IdGenero, Portada= @PortadaURL WHERE Id= @Id
-GO*/
-
-/*create procedure sp_altaCliente
-	@mail varchar(500), 
-	@contraseña varchar (500),
-	@nombres varchar (100),
-	@apellidos varchar (100),
-	@DNI varchar (50),
-	@telefono varchar (100), 
-	@celular varchar (100),
-	@calle varchar (100),
-	@numero varchar (10),
-	@piso varchar (10),
-	@departamento varchar (10),
-	@CP varchar (10),
-	@localidad varchar (100),
-	@provincia varchar (100)
-as 
-insert into Clientes values (@mail,	@contraseña, @nombres, @apellidos, @DNI, @telefono, @celular, @calle, @numero, @piso, @departamento, @CP, @localidad, @provincia, 1)
-go*/
+GO
 
 
-alter table Clientes
-add Estado bit not null default(1);
-go
-
-
-/*CREATE PROCEDURE SP_EliminarFisico
+CREATE PROCEDURE SP_EliminarFisico
 	@Id smallint 
 AS 
 DELETE FROM Libros WHERE Id = @Id
+GO
 
-GO*/
 
-/*CREATE PROCEDURE SP_EliminarLogico
-@Id smallint 
-AS 
-UPDATE Libros SET Estado= 0 WHERE Id = @Id
-
-GO*/ 
-
-/*CREATE PROCEDURE SP_ListarLibrosInactivos
-as
-select l.id, l.Titulo, l.Descripcion, l.Autor, l.Editorial, l.Precio, l.Stock, g.IdGenero as Genero_ID, g.Descripcion as Genero_Desc, l.Portada, l.estado 
-from Libros l 
-inner join Generos g on l.IdGenero = g.IdGenero 
-
-GO*/ 
-
-/*CREATE PROCEDURE SP_EliminarLogico
+CREATE PROCEDURE SP_EliminarLogico
 @Id smallint, 
 @Estado bit
 AS 
 UPDATE Libros SET Estado= @Estado WHERE Id = @Id
-go*/
-
-/*create procedure sp_listarClientes as
-select 
-	c.IdCliente, 
-	c.mail, 
-	c.Contraseña, 
-	c.Nombres, 
-	c.Apellidos, 
-	c.Dni, 
-	c.Telefono, 
-	c.Celular, 
-	c.Calle, 
-	c.Numero, 
-	c.Piso, 
-	c.Departamento, 
-	c.CP, 
-	c.Localidad, 
-	c.Provincia, 
-	c.estado
-from clientes c
-go*/
-
-/*create procedure sp_modificarCliente
-@idCliente smallint, 
-@contraseña varchar(500),
-@nombres varchar(100),
-@apellidos varchar(100),
-@dni varchar (50),
-@telefono varchar (100),
-@celular varchar (100),
-@calle varchar (100),
-@numero varchar (10),
-@piso varchar (10),
-@departamento varchar (10),
-@cp varchar (10),
-@localidad varchar (100),
-@provincia varchar (100),
-@estado bit
-as
-update Clientes set Contraseña = @contraseña, Nombres = @nombres, Apellidos = @apellidos, DNI = @dni, Telefono = @telefono, 
-Celular = @celular, Calle = @calle, Numero = @numero, Piso = @piso, Departamento = @departamento, CP = @cp, Localidad = @localidad, Provincia = @provincia, Estado = @estado
-where IdCliente = @idCliente
-go*/
-
-
-/*create procedure sp_ClienteEliminarFisico
-	@idCliente smallint
-as
-delete from clientes 
-where idCliente = @idCliente
-go*/
-
-alter table clientes
-add TipoUser int default (1)
-go
-
-alter table administradores
-add TipoUser int default (2)
 go
 
 
-/*create procedure sp_login(
-	@mail varchar (500), 
-	@pass varchar (500)
-)
+CREATE PROCEDURE SP_ListarLibrosInactivos
 as
-select 
-	u.IdUsuario,
-	u.TipoUsuario,
-	u.Apellidos,
-	u.Nombres
-from usuarios u
-where u.Mail = @mail 
-and u.Contraseña = @pass
-go*/
+select l.id, l.Titulo, l.Descripcion, l.Autor, l.Editorial, l.Precio, l.Stock, g.IdGenero as Genero_ID, g.Descripcion as Genero_Desc, l.Portada, l.estado 
+from Libros l 
+inner join Generos g on l.IdGenero = g.IdGenero 
+GO 
 
 
-/*create table usuarios (
+--TABLAS PARA USUARIOS
+create table usuarios (
 	IdUsuario smallint primary key identity (1,1),
 	Mail varchar(500) unique, 
 	Contraseña varchar (500) Not Null,
@@ -243,6 +97,7 @@ go*/
 	TipoUsuario smallint not null
 )
 go
+
 
 create table datos_usuario (
 	IdUsuario smallint primary key foreign key references usuarios(IdUsuario), 
@@ -257,10 +112,27 @@ create table datos_usuario (
 	Localidad varchar (100),
 	Provincia varchar (100)
 )
-go*/
+go
 
 
-/*create procedure sp_insertarNuevo(
+--PROCEDIMIENTOS PARA USUARIOS
+create procedure sp_login(
+	@mail varchar (500), 
+	@pass varchar (500)
+)
+as
+select 
+	u.IdUsuario,
+	u.TipoUsuario,
+	u.Apellidos,
+	u.Nombres
+from usuarios u
+where u.Mail = @mail 
+and u.Contraseña = @pass
+go
+
+
+create procedure sp_insertarNuevo(
 	@Apellidos varchar (100),
 	@Nombres varchar (100),
 	@Mail varchar (500),
@@ -273,9 +145,10 @@ begin
 	output inserted.IdUsuario
 	values (@Apellidos, @Nombres, @Mail, @Contraseña, @TipoUsuario, 1)
 end
-go*/
+go
 
-/*create procedure sp_listarClientes
+
+create procedure sp_listarClientes
 as
 select 
 	u.IdUsuario,
@@ -296,7 +169,6 @@ select
 from usuarios u
 left join datos_usuario du on u.IdUsuario = du.IdUsuario
 go
-
 
 
 create procedure sp_modificarCliente(
@@ -341,6 +213,7 @@ begin
 end
 go
 
+
 create procedure sp_usuarioEliminarLogico (
 	@IdUsuario smallint, 
 	@activo bit
@@ -352,6 +225,7 @@ begin
 	where IdUsuario = @IdUsuario
 end
 go
+
 
 create procedure sp_UsuarioEliminarFisico (
 	@idUsuario smallint
@@ -401,4 +275,29 @@ from usuarios u
 left join datos_usuario du on u.IdUsuario = du.IdUsuario
 where u.IdUsuario = @idUsuario
 end
-go*/
+go
+
+
+--TABLAS PARA VENTAS
+CREATE TABLE Ventas(
+    IDVenta int PRIMARY KEY IDENTITY(1,1),
+    IDUsuario smallint not null FOREIGN KEY REFERENCES Usuarios (IdUsuario),
+    FormaPago char not null CHECK (FormaPago='E' OR FormaPago='T'),
+    Envio bit not null,
+    Importe money not null CHECK(Importe>0),
+    Cantidad int not null CHECK (Cantidad>0),
+    Fecha datetime not null,
+    Estado char not null CHECK (Estado='R' OR Estado='P' OR Estado='E' OR Estado='C')
+)
+GO
+
+CREATE TABLE ItemCarrito(
+    IDItem smallint not null,
+    NombreItem varchar (200) not null,
+    Cantidad int not null Check(Cantidad >0),
+    Precio money not null Check(Precio >0),
+    IDVenta int not null FOREIGN KEY REFERENCES Ventas (IDVenta)
+)
+GO
+
+--PROCEDIMIENTOS PARA VENTAS
